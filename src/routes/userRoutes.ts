59b43cbe8faee2fa95bb73bb25c15eddb675d436
aaryan359 <aaryanmeena96@gmail.com>
2025-05-04 12:54:17 +0530

    updated authrouter

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { login, register } from "../contorllers/authController";
+import { login, register, updateProfile } from "../contorllers/authController";
 import { validateRequest } from "../middlewares/validationMiddleware";
 import { registerSchema } from "../validations/authValidations";
+import { authMiddleware } from "../middlewares/authmiddleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.patch('/profile',authMiddleware,updateProfile);
 
 export default router;
